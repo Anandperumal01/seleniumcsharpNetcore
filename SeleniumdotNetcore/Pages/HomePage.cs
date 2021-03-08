4aa9b350f7b717690d8db236b419ac8f0c273088
Anandperumal01 <anandperumalece@gmail.com>
2021-03-09 02:43:11 +0530

    Added the page object model

---
+++ SeleniumdotNetcore/Pages/HomePage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumdotNetcore.Pages
+{
+    class HomePage :DriverHelper
+    {
+
+        IWebElement lnkLogin => driver.FindElement(By.LinkText("Login"));
+        
+        IWebElement lnkLogoff => driver.FindElement(By.LinkText("Log off"));
+
+        public void clickLogin() => lnkLogin.Click();
+
+        public bool isLogoffExist() => lnkLogoff.Displayed;
+    }
+}
---
+++ SeleniumdotNetcore/Pages/LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumdotNetcore.Pages
+{
+    public class LoginPage : DriverHelper
+    {
+
+        IWebElement txtUserName => driver.FindElement(By.Id("UserName"));
+
+
+        IWebElement txtPassword => driver.FindElement(By.Id("Password"));
+
+        IWebElement btnLogin => driver.FindElement(By.XPath("//input[@type='submit']"));
+
+
+
+        public void enterUserNameAndPassword(string userName, string password)
+        {
+            txtUserName.SendKeys(userName);
+            txtPassword.SendKeys(password);
+
+
+
+        }
+
+
+        public void ClickLogin()
+        {
+
+            btnLogin.Click();
+
+        }
+    }
+}
